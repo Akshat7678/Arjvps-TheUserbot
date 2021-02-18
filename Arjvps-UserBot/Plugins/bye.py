f86a5c5f2303263e8b012ca714a0af042beb39a9
/\k$h@t <73880884+Akshat7678@users.noreply.github.com>
2021-02-18 10:26:08 +0530

    Create bye.py

---
+++ Arjvps-UserBot/Plugins/bye.py
@@ -0,0 +1,31 @@
+"""
+.bye
+"""
+
+
+import time
+
+from telethon.tl.functions.channels import LeaveChannelRequest
+
+from telebot import CMD_HELP
+from telebot.utils import admin_cmd
+
+
+@telebot.on(admin_cmd(pattern="bye", outgoing=True))
+async def leave(e):
+    x = bot.me
+    name = x.first_name
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+
+        await e.edit(f"`{name} has left this group, bye!!.`")
+
+        time.sleep(3)
+
+        if "-" in str(e.chat_id):
+
+            await borg(LeaveChannelRequest(e.chat_id))
+
+        else:
+
+            await e.edit("`This is Not A Chat. Please use this in groups :/`")
+
