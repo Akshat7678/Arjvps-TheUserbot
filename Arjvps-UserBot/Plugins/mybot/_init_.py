0e5acc09335554917a13ba5fa0333c2159ca6402
/\k$h@t <73880884+Akshat7678@users.noreply.github.com>
2021-02-18 11:06:28 +0530

    Create _init_.py

---
+++ Arjvps-UserBot/Plugins/mybot/_init_.py
@@ -0,0 +1,59 @@
+from telebot.plugins import OWNER_ID, TELE_NAME
+import time
+from datetime import datetime
+from telebot.telebotConfig import Var
+
+# start-other disabled
+startotherdis = """
+Hi there. I am {}'s bot. Nice to see you here.
+""".format(TELE_NAME)
+
+# start-other enabled
+if Var.PMBOT_START_MSSG is None:
+    MSSG = """
+Hi there, I am {}'s personal bot.
+You can contact him through me 😌.
+Have a nice time!
+""".format(TELE_NAME)
+else:
+    MSSG = Var.PMBOT_START_MSSG
+startotherena = MSSG
+
+# start-owner
+startowner = """
+Welcome back {}. Choose the options available from below:
+""".format(TELE_NAME)
+
+# for ping
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+
+
+xstart = datetime.now()
+xend = datetime.now()
+ms = (xend - xstart).microseconds / 1000
+ping = f"🏓Pong\nPing speed: {ms}"
